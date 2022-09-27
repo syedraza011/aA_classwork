35eb39c11f233a214712adf39e6b8b50d0d6454e
Syed Raza <syed.raza011@gmail.com>
2022-09-27 19:42:35 -0400

    Question 7 completed

--- week_01/d2Hw2.js
+++ week_01/d2Hw2.js
@@ -1,67 +1,3 @@
-// //////day _02 class work////
-// if (!0)
-// {
-//     console.log("value is true ");
-
-// }
-// else{
-//     console.log("else: value is false");
-// }
-
-// let num=20;
-// if(num=30){
-// console.log("in the if");
-// }
-// else{
-//     console.log("in the else");
-// }
-// /*functions
-// dry code means don't repeat yourself
-// it is a procedure of code that will run when called*/
-// function foo()
-// {
-//     return 'bar';
-// }
-// console.log(foo()); // calling the value of funtion
-// console.log(foo); // calling what is it
-
-// function foo(p1, p2)
-// {
-//     return p1 + " "+p2;
-// }
-// console.log(foo("Mr","Raza"));
-
-// //way to declare functions
-
-// let too = function (){
-//     return "too function";
-// }
-// console.log(too());
-
-// let doo = ()=>
-// {
-// return "doo function"
-// }
-// console.log(doo());
-
-/*console.log(avg(2,4));
-function avg(num1, num2)
-{
-    let sum = (num1 + num2) / 2;
-    return sum;
-}
-//console.log(avg(3,4));
-console.log(avg(2,2));
-function avg1(num1,num2)
-{
-    let sum= sumNum(2,2);
-    return avg=sumNum/2;
-}
-function sumNum (num1,num2){
-    let sum= num1+num2;
-    return sum;
-}
-
 
 // Write a function `isOdd` that takes in a number as an argument and returns `true`
 // if the number is odd and returns `false` otherwise.
@@ -180,7 +116,7 @@ function whisper(string)
     str=str.toLowerCase(str);
     return str;
 }
-*/
+//07
 // Write a function `isSubstring` that takes in two strings, `searchString` and
 // `subString`. The function should return `true` if `subString` is a part of the
 // `searchString`, `false` otherwise.
@@ -204,3 +140,4 @@ function isSubstring(searchString, subString) {
         return false;
     }
 }
+//8
