f7b5b49d0a42c67b06e775a46973fb48c5962367
Syed Raza <syed.raza011@gmail.com>
2022-09-27 18:52:15 -0400

    day 2 updates

--- week_01/day_02.js
+++ week_01/day_02.js
@@ -1,3 +1,46 @@
+//////day _02 class work////
+/*if (!0)
+{
+    console.log("if");
+
+}
+else{
+    console.log("else");
+}
+*/
+let num=20;
+if(num=30){
+console.log("in the if");
+}
+else{
+    console.log("in the else");
+}
+/*functions
+dry code means don't repeat yourself
+it is a procedure of code that will run when called*/
+function foo()
+{
+    return 'bar';
+}
+console.log(foo()); // calling the value of funtion
+console.log(foo); // calling what is it
+
+function foo(p1, p2)
+{
+    return p1 + " "+p2;
+}
+console.log(foo("syed","Raza"));
+
+
+
+
+
+
+
+
+
+
+/*
 // Write a function `isOdd` that takes in a number as an argument and returns `true`
 // if the number is odd and returns `false` otherwise.
 // Write two versions of this function, using conditionals and without using conditionals
@@ -83,3 +126,36 @@ function hello(str) {
   }
 
 hello(str);
+
+//5
+// Write a function `yell` that takes in a string and returns a "yelled" version
+// of that string.
+//
+// Examples:
+// yell("I want to go to the store"); // => "I WANT TO GO TO THE STORE!!!"
+// yell("Time to program"); // => "TIME TO PROGRAM!!!"
+//
+var str= "i am going to app academy";
+console.log(yell(str));
+function yell(string) 
+{
+    str=str.toUpperCase(str);
+    return str;
+}
+  //6
+
+  // Write a function `whisper` that takes in a string and returns a "whispered" version
+// of that string.
+//
+// Examples:
+// whisper("Hey Anthony"); // => "...hey anthony..."
+// whisper("YEA! that was fun"); // => "...yea! that was fun..."
+
+var str= "i am GOING to app academy";
+console.log(whisper(str));
+function whisper(string) 
+{
+    str=str.toLowerCase(str);
+    return str;
+}
+*/
