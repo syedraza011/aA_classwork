4f8b557f52d7997c6ff741f26464b8e004a1b25d
Syed Raza <syed.raza011@gmail.com>
2022-09-28 13:00:50 -0400

    question 6 completed

--- week_01/d3H3.js
+++ week_01/d3H3.js
@@ -160,3 +160,28 @@ function sumNums(maximum){
     }
     console.log(sum);
 }
+
+//6
+
+// Define a function `isFactorOf(number, factor)` that returns `true` if `factor`
+// is a factor of `number`. `false` otherwise. A factor is a number that divides
+// another number without a leftover remainder.
+//
+// Examples:
+//
+// isFactorOf(6,2); // => true
+// isFactorOf(-6, 2); // => true
+// isFactorOf(5,0); // => false
+// isFactorOf(22, 7); // => false
+let number1=5;
+let factor=0;
+console.log(isFactorOf(number1, factor));
+function isFactorOf(number1, factor){
+    if(number1 % factor===0)
+    {
+        return true;
+    }
+    else{
+        return false;
+    }
+}
