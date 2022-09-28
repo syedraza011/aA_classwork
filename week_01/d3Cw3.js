b2f21b7e9da07fea9db3223952d70d7a6f2bbfe9
Syed Raza <syed.raza011@gmail.com>
2022-09-28 19:27:02 -0400

    another update on loops

--- week_01/d3Cw3.js
+++ week_01/d3Cw3.js
@@ -1,5 +1,5 @@
 //loops
-
+/*
 for(let i = 0; i <= 10; i++){
     console.log(i);
 }
@@ -9,10 +9,24 @@ for(let i = 1; i <= 10; i++){
 console.log("after the loop");
 
 let strt=3;
-let end=9;
+let end=10;
 printRang(strt,end);
 function printRang(strt,end){
     for(let i = strt; i <= end; i++){
         console.log(i);
     }
 }
+*/
+//While loop
+let nStrt = 7;
+let nEnd = 21;
+console.log("While loop");
+printRang(nStrt,nEnd);
+function printRang(nStrt,nEnd){
+    let i = nStrt;
+    while( i <= nEnd){
+        console.log(i);
+        i++;
+    }
+}
+
