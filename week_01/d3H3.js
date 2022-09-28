52969262a2d6f0333782263bf5e7f4296664eb4c
Syed Raza <syed.raza011@gmail.com>
2022-09-28 12:42:46 -0400

    question 5 completed

--- week_01/d3H3.js
+++ week_01/d3H3.js
@@ -139,3 +139,24 @@ function printReverse(min, max){
         console.log("Fix the values");
     }
 }
+
+//5
+// Write a function `sumNums(max)` that returns the sum of all positive numbers less
+// than or equal to max.
+//
+// Examples:
+//
+// sumNums(4); // => 10
+// sumNums(10); // => 55
+// sumNums(365);
+let sum=0;
+let maximum=365;
+sumNums(maximum); 
+function sumNums(maximum){
+    for(let j=0;j<=maximum;j++){
+        //console.log(j);
+    sum=sum+j;
+       // console.log(sum);
+    }
+    console.log(sum);
+}
