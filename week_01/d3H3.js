ad3c7675576af35543f257f2b8a505bacb65228d
Syed Raza <syed.raza011@gmail.com>
2022-09-28 13:10:49 -0400

    question 7 completed

--- week_01/d3H3.js
+++ week_01/d3H3.js
@@ -185,3 +185,27 @@ function isFactorOf(number1, factor){
         return false;
     }
 }
+//7
+// Define a function `fizzBuzz(max)` that takes a number and prints  every number
+// from 0 to `max` that is divisible by either 3 or 5, but not both.
+//
+// Examples:
+//
+// fizzBuzz(20); // prints
+// 3
+// 5
+// 6
+// 9
+// 10
+// 12
+// 18
+let max=20;
+let j=0;
+    fizzBuzz(max);
+function fizzBuzz(max){
+    for(j=0;j<max;j++){
+        if(j%3===0 && j%5!==0 || j%3!==0 && j%5 ===0){
+            console.log(j);
+        }
+    }
+}
