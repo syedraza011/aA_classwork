4027c3c3d0660c5d937b7da17679aac8725486f0
Syed Raza <syed.raza011@gmail.com>
2022-09-27 20:42:49 -0400

    class work updated

--- week_01/d2Cw2.js
+++ week_01/d2Cw2.js
@@ -1,50 +1,50 @@
 ///day _02 class work//
-// if (!0)
-// {
-//     console.log("value is true ");
+if (!0)
+{
+    console.log("value is true ");
 
-// }
-// else{
-//     console.log("else: value is false");
-// }
+ }
+else{
+    console.log("else: value is false");
+}
 
-// let num=20;
-// if(num=30){
-// console.log("in the if");
-// }
-// else{
-//     console.log("in the else");
-// }
-// /*functions
-// dry code means don't repeat yourself
-// it is a procedure of code that will run when called*/
-// function foo()
-// {
-//     return 'bar';
-// }
-// console.log(foo()); // calling the value of funtion
-// console.log(foo); // calling what is it
+let num=20;
+if(num=30){
+console.log("in the if");
+}
+else{
+    console.log("in the else");
+}
+/*functions
+dry code means don't repeat yourself
+it is a procedure of code that will run when called*/
+function foo()
+{
+    return 'bar';
+}
+console.log(foo()); // calling the value of funtion
+console.log(foo); // calling what is it
 
-// function foo(p1, p2)
-// {
-//     return p1 + " "+p2;
-// }
-// console.log(foo("Mr","Raza"));
+function foo(p1, p2)
+{
+    return p1 + " "+p2;
+}
+console.log(foo("Mr","Raza"));
 
-// //way to declare functions
+//way to declare functions
 
-// let too = function (){
-//     return "too function";
-// }
-// console.log(too());
+let too = function (){
+    return "too function";
+}
+console.log(too());
 
-// let doo = ()=>
-// {
-// return "doo function"
-// }
-// console.log(doo());
+let doo = ()=>
+{
+return "doo function"
+}
+console.log(doo());
 
-/*console.log(avg(2,4));
+console.log(avg(2,4));
 function avg(num1, num2)
 {
     let sum = (num1 + num2) / 2;
@@ -60,4 +60,4 @@ function avg1(num1,num2)
 function sumNum (num1,num2){
     let sum= num1+num2;
     return sum;
-}*/
+}
--- week_01/d2Hw2.js
+++ week_01/d2Hw2.js
@@ -217,13 +217,16 @@ function checkNumber(num) {
   console.log(checkNumber(-5)); // feel free to change the number being passed
 
 //13
-console.log(1 + 1 * 5)
+console.log(1 + 1 * 5); //answer is 6
 
-console.log((1 + 1) * 5)
+console.log((1 + 1) * 5);// 10
 
-//1 + 2 - 5 / 6 - 1
+console.log(1 + 2 - 5 / 6 - 1); //1.16999
 
-//5 * 5 % 13
-
-//5/(-1 * (5 - (-10)))
+console.log(5 * 5 % 13)// 12
+//console.log(25%13)// 12 testing how it got answer
+console.log(5/(-1 * (5 - (-10))));//-0.33
+console.log(5/(-1 * (5+10)));//steps
+console.log(5/(-1 * 15));//steps
+console.log(5/-15); //steps
 
