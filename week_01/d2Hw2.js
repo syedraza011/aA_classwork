2abe433a508e8b0779630f6db5aa4b7d6e9e2a58
Syed Raza <syed.raza011@gmail.com>
2022-09-27 20:15:18 -0400

    problem 9 solved

--- week_01/d2Hw2.js
+++ week_01/d2Hw2.js
@@ -150,10 +150,7 @@ function isSubstring(searchString, subString) {
 // echo("JUMp"); // => "JUMP ... JUMp ... jump"
 //
 var word="Mom!";
-//console.log(echo());
-
-    
-    console.log(echo(word));
+console.log(echo(word));
 function echo(word) {
     
     let part1 = word.toUpperCase();
@@ -163,3 +160,20 @@ function echo(word) {
     return newWord;
 
 }
+// Write a function `isEven` that takes in a number and returns `true` if the number
+// is even, `false` otherwise. Write `isEven` by using the `isOdd` function you
+// made previously. HINT: you can call `isOdd` from within `isEven`.
+//
+// Examples:
+// isEven(2); // => true
+// isEven(5); // => false
+let number=1;
+console.log(isEven(number));
+function isEven(number) {
+    if(number % 2 === 0){
+        return true;
+    }
+    else{
+        return false;
+    }
+  }
