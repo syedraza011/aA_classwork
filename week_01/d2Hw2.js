370cbfb676b8ab1c92b4726bc0391c0317b06e3c
Syed Raza <syed.raza011@gmail.com>
2022-09-27 20:19:32 -0400

    problem 10  solved

--- week_01/d2Hw2.js
+++ week_01/d2Hw2.js
@@ -177,3 +177,21 @@ function isEven(number) {
         return false;
     }
   }
+
+  // Write a function `averageOfFour(num1, num2, num3, num4)` that takes in four
+// numbers. The function should return the average of all of the numbers.
+//
+// Examples:
+//
+// averageOfFour(10, 10, 15, 5); // => 10
+// averageOfFour(3, 10, 11, 4); // => 7
+// averageOfFour(1, 2, 3, 4); // => 2.5
+let num11=4;
+let num12=2;
+let num13=2;
+let num14=4;
+console.log(averageOfFour(num11,num12,num13,num14));
+function averageOfFour(num1, num2, num3, num4){
+    avg=(num11+num12+num13+num14)/4;
+    return avg;
+}
