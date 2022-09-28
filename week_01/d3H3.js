e950b4eed790038c5d69504448079dfa977f0404
Syed Raza <syed.raza011@gmail.com>
2022-09-27 23:00:33 -0400

    D3 home work

---
+++ week_01/d3H3.js
@@ -0,0 +1,19 @@
+// Define a function `logBetween(lowNum, highNum)` that will print every number
+// from `lowNum` to `highNum`, inclusive. Inclusive means that the range includes
+// `lowNum` and `highNum`. Hint: this function only needs to print using `console.log`,
+// it does not need to return.
+//01
+// Examples:
+//
+// logBetween(-1, 2); // prints
+// -1
+// 0
+// 1
+// 2
+//
+// logBetween(14, 6); // prints nothing
+//
+// logBetween(4,6); // prints
+// 4
+// 5
+// 6
