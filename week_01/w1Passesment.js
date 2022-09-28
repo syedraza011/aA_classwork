675e55689e2328dbf3d0bee9a1bbe07ce34b2dd4
Syed Raza <syed.raza011@gmail.com>
2022-09-28 17:59:55 -0400

    week 1 assesment practice question 2

--- week_01/w1Passesment.js
+++ week_01/w1Passesment.js
@@ -19,3 +19,26 @@ function average(num1, num2) {
   module.exports = average
 
   //2
+
+  /*******************************************************************************
+** Write a function tripler(array) that takes in an array and returns a new array
+** containing 3 times every element of the original array.
+**
+** Examples:
+**
+** tripler([1,2,3]); // => [ 3, 6, 9 ]
+** tripler([4, 1, 7]); // => [ 12, 3, 21 ]
+*/
+let tArray=[4,1,7];
+tripler(tArray);
+function tripler(TArray) {
+  for(let i=0;i<tArray.length;i++){
+    tArray[i]=tArray[i]*3;
+    console.log(tArray[i]);
+  }
+  
+}
+
+
+/**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*************************/
+module.exports = tripler
