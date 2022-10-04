c8190dc3caba712a0f61312e4fa4f13bc4126b95
Syed Raza <syed.raza011@gmail.com>
2022-10-04 18:46:02 -0400

    class work updates

--- week_02/w2d2_pset/w2d2_cw.js
+++ week_02/w2d2_pset/w2d2_cw.js
@@ -1,7 +1,33 @@
 //class work
 //Objects
-catBuilder(whiskers,black,yarn)
+console.log(catBuilder("whiskers","black","yarn"));
 function catBuilder(name,color,toys){
-let cat={};
-cat.name=name;
+let cat={
+/*cat.name=name;
+cat.color=color;
+//2nd way of doing
+
+cat["name"]=name;
+cat["color"]=color; // we can only key in objects when they are string
+return cat
+}*/
+//3rd way of doing
+  name: name,
+  color: color,
+  toys: toys,
+};
+return cat;
 }
+//2nd example
+console.log(catBuilder("whiskers","black","yarn"));
+function catBuilder(name,color,toys){
+  return{
+    name: name,
+    color: color,
+    toys: toys,
+  };
+  //return cat;
+  }
+
+  //fucntion decompostion and objects
+  
