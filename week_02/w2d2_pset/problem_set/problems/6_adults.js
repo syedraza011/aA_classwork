e765926bc1ed167204720f3b453beae99abb6b41
Syed Raza <syed.raza011@gmail.com>
2022-10-05 13:36:03 -0400

    Question 6 done

--- week_02/w2d2_pset/problem_set/problems/6_adults.js
+++ week_02/w2d2_pset/problem_set/problems/6_adults.js
@@ -14,9 +14,24 @@ var ppl = [
 
 adults(ppl); // => [ 'John', 'Jane' ]
 ***********************************************************************/
-
+let people = [
+  {name: 'John', age: 20},
+  {name: 'Jim', age: 13},
+  {name: 'Jane', age: 18},
+  {name: 'Bob', age: 7}
+];
+console.log(adults(people));
 function adults(people) {
-
+  let array=[];
+  let person='';
+for( let i=0;i<people.length ;i++){
+  person=people[i];
+  if(person.age >= 18){
+    //console.log(person);
+    array.push(person.name);
+  }
+}
+return array;
 }
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
