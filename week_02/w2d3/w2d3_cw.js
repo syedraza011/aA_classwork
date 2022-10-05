bbf0ea647fca7b0b6601a693cf5b0f83ccb0452f
Syed Raza <syed.raza011@gmail.com>
2022-10-05 18:56:41 -0400

    qnother question target value

--- week_02/w2d3/w2d3_cw.js
+++ week_02/w2d3/w2d3_cw.js
@@ -9,7 +9,8 @@ function everycombination(){
     }
   }
 }
-//
+// 
+console.log("New Example")
 let students= [`kevin`, `jesse`, `jason`, `simson`];
 //let name= `jael`;
 //console.log( `Hi my name is ${name}`);
@@ -17,10 +18,22 @@ pairstudent(students);
 function pairstudent(arr){
   for(let i=0;i<arr.length;i++){
     let student1=arr[i];
-    for(let j=i+1;j<arr.length;j++){
+    for(let j=i;j<arr.length;j++){
       let student2=arr[j];
       console.log(`${student1} will be paired with ${student2}`);
     }
   }
 }
-
+let arr=[0,1,2,3,4,5,6]
+let target=5;
+//pointers
+toSum(arr,target);
+ function toSum(arr,target){
+  for(let i=0;i<arr.length;i++){
+    for(let j=i+1; j<arr.length;j++){
+      if(arr[i]+arr[j]===target){
+        console.log([i,j]);
+      }
+    }
+  }
+ }
