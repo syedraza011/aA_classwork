9516fad6ad1e063b5925b9fd6b299cfc9340ab32
Syed Raza <syed.raza011@gmail.com>
2022-10-05 19:44:35 -0400

    final question

--- week_02/w2d3/w2d3_cw.js
+++ week_02/w2d3/w2d3_cw.js
@@ -26,7 +26,7 @@ function pairstudent(arr){
 }
 let arr=[0,1,2,3,4,5,6]
 let target=5;
-//pointers
+// loops
 toSum(arr,target);
  function toSum(arr,target){
   for(let i=0;i<arr.length;i++){
@@ -37,3 +37,57 @@ toSum(arr,target);
     }
   }
  }
+
+ // Matrix //nested array
+ let Matrix=[
+  [1,2,3], //matrix[0]
+  [4,5,6], //matrix[1]
+  [7,8,9] //matrix[2]
+ ]
+ for(let i=0;i<Matrix.length;i++){
+  let subArray=Matrix[i]
+  for(let j=0;j<subArray.length;j++){
+    let ele=Matrix[i][j];
+    console.log(ele);
+  }
+ }
+
+ //nested array
+ let plane=[
+  [0,1,2,3,0],
+  [1,4,3,1],
+  [1,5,6,4],
+  [0,4,3,2]
+  ]
+  console.log("Biggest Cordinate");
+  console.log(getHighestCordinateOnAPlane(plane));
+  function getHighestCordinateOnAPlane(plane){
+    let biggest=plane[0][0];
+    let currentBiggestIndex=[0,0];
+    for(let i = 0; i < plane.length; i++){
+      let subArray=plane[i];
+      for(let j = 0; j < subArray.length ;j++){
+        if(subArray[j]>biggest){
+          biggest=subArray[j];
+          currentBiggestIndex=[i,j];
+        }
+      }
+    }
+    return currentBiggestIndex;
+ }
+ //
+
+ let array=[
+  { name:`Ry`, favFood:[`brownies`,'sushi', 'tacos'] },
+  {name:`Brookie`, favFood:[`Apples`,'Banana', 'Pears']}
+ ]
+ printFavoriteFood(array);
+function printFavoriteFood(array){
+  for(let i = 0; i < array.length; i++ ){
+    let obj=array[i];
+    let arrayInObj=obj.favFood;
+    for(let j = 0; j<arrayInObj.length; j++){
+      console.log(arrayInObj[j]);
+    }
+  }
+}
