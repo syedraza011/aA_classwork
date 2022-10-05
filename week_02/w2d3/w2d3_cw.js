beb5d2c333a7502f84c2e5c53c42dcb07b4c7bb2
Syed Raza <syed.raza011@gmail.com>
2022-10-05 18:48:36 -0400

    nested loop working with function

--- week_02/w2d3/w2d3_cw.js
+++ week_02/w2d3/w2d3_cw.js
@@ -13,3 +13,14 @@ function everycombination(){
 let students= [`kevin`, `jesse`, `jason`, `simson`];
 //let name= `jael`;
 //console.log( `Hi my name is ${name}`);
+pairstudent(students);
+function pairstudent(arr){
+  for(let i=0;i<arr.length;i++){
+    let student1=arr[i];
+    for(let j=i+1;j<arr.length;j++){
+      let student2=arr[j];
+      console.log(`${student1} will be paired with ${student2}`);
+    }
+  }
+}
+
