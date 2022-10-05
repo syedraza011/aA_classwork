f5b752176b940c54c6cbfb9cd0deb230eeb92069
Syed Raza <syed.raza011@gmail.com>
2022-10-05 13:40:45 -0400

    Question 7

--- week_02/w2d2_pset/problem_set/problems/7_has_favorite_food.js
+++ week_02/w2d2_pset/problem_set/problems/7_has_favorite_food.js
@@ -21,7 +21,11 @@ var person = {
 hasFavoriteFood(person, 'burgers'); // => true
 hasFavoriteFood(person, 'fish'); // => false
 ***********************************************************************/
-
+let dog = {
+  name: 'Fido',
+  favoriteFoods: ['chicken', 'sausage', 'scooby snacks']
+}
+console.log(hasFavoriteFood(obj, food));
 function hasFavoriteFood(obj, food) {
 
 }
