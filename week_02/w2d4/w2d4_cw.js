ecb6e85856e58f3be2a7dcfb68874dc6c2f24b20
Syed Raza <syed.raza011@gmail.com>
2022-10-06 18:35:58 -0400

    lecture update

--- week_02/w2d4/w2d4_cw.js
+++ week_02/w2d4/w2d4_cw.js
@@ -1,4 +1,4 @@
-// Strings vs arrays
+// Strings vs arrays //.index, .includes,.concat,.slice
 //strings- primitive, non muteable
 //array=non primitive , muteable
 let str="hello";
@@ -7,3 +7,7 @@ console.log(str);
 let arr=['h','e','l','l','o'];
 arr[0]='x';
 console.log(arr);
+arr.join('++')
+console.log(arr.join('++')); // add this to after every index and tur
+
+console.log(str.split(" "));
