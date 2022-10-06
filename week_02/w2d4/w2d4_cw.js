1e88e79f6063af749926c4720fdb0c00a38511f7
Syed Raza <syed.raza011@gmail.com>
2022-10-06 18:30:23 -0400

    Class work

--- week_02/w2d4/w2d4_cw.js
+++ week_02/w2d4/w2d4_cw.js
@@ -1,1 +1,9 @@
 // Strings vs arrays
+//strings- primitive, non muteable
+//array=non primitive , muteable
+let str="hello";
+str[0]=["x"];
+console.log(str);
+let arr=['h','e','l','l','o'];
+arr[0]='x';
+console.log(arr);
