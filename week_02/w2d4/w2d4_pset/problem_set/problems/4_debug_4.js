a751acae8a3a8c5a0b2179f1ff7d29cba61b748d
Syed Raza <syed.raza011@gmail.com>
2022-10-06 20:32:57 -0400

    Question 4

--- week_02/w2d4/w2d4_pset/problem_set/problems/4_debug_4.js
+++ week_02/w2d4/w2d4_pset/problem_set/problems/4_debug_4.js
@@ -5,17 +5,19 @@
 // isMagicNumer(26) //=> true;
 // isMagicNumer(50) //=> false;
 //let number=26;
-let magicify = function(number) {
-  return console.log(number- 34);
-}
+let magicify = function (number) {
+  return number - 34;
+};
 
-let isMagicNumer = function(number) {
+let isMagicNumer = function (number) {
   let magicNumb = magicify(number);
   if (magicNumb % 13 === 0 || magicNumb < 0) {
     return true;
   }
   return false;
-}
+};
+console.log(isMagicNumer(26)); //=> true;
+console.log(isMagicNumer(50)); //=> false;
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 module.exports = isMagicNumer;
