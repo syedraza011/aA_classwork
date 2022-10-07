ef6bd942e0e7c78052bc89275314f7f3083b3e80
Syed Raza <syed.raza011@gmail.com>
2022-10-07 14:00:23 -0400

    Question 3 day 5 week 2 done

--- week_02/w2d5/problem_set/problems/3_lcm.js
+++ week_02/w2d5/problem_set/problems/3_lcm.js
@@ -8,11 +8,22 @@ lcm(2, 3); // => 6
 lcm(6, 10); // => 30
 lcm(24, 26); // => 312
 ***********************************************************************/
-
+let num1 = 2;
+let num2 = 3;
+console.log(lcm(num1, num2));
 function lcm(num1, num2) {
+    let i = num1;
+    while (true) {
+        if (i % num2 === 0) {
+            return i;
+        } 
+        
+        i+=num1;
+    }
 
+       // return flase;
+    
 }
-
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 module.exports = lcm;
