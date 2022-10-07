bd08722515fb94b253b51b4f2b72e215ef32bb5a
Syed Raza <syed.raza011@gmail.com>
2022-10-06 20:29:45 -0400

    Questions update

--- week_02/w2d4/w2d4_pset/problem_set/problems/6_cap_vowels.js
+++ week_02/w2d4/w2d4_pset/problem_set/problems/6_cap_vowels.js
@@ -11,9 +11,25 @@ capVowels('hello world'); // => 'hEllO wOrld'
 capVowels('HELLO WORLD'); // => 'hEllO wOrld'
 capVowels('boOtCamP PreP'); // => 'bOOtcAmp prEp'
 ***********************************************************************/
-
+let string='hello world';
+console.log(capVowels(string)); 
 function capVowels(string) {
-
+    let newArray=[];
+    let words=string.split(" ");
+    for(let i=0;i<string.length;i++){
+        let word=string[i];
+            let char=word.split(" ");
+        for(let j=0;j<word.length;j++){
+            if(char==='e'){
+                char.toUpperCase(char[j]);
+                newArray.push(char);
+            }
+            else{
+                newArray.push(char);
+            }
+        }
+    }
+return newArray.join(" ");
 }
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
