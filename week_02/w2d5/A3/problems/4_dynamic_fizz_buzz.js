58bad85a42b55eae042cdf29e235064f7b293b8a
Syed Raza <syed.raza011@gmail.com>
2022-10-07 18:56:08 -0400

    Question 5 done

--- week_02/w2d5/A3/problems/4_dynamic_fizz_buzz.js
+++ week_02/w2d5/A3/problems/4_dynamic_fizz_buzz.js
@@ -8,13 +8,18 @@ Examples:
 dynamicFizzBuzz(20, 5, 3) => [3, 5, 6, 9, 10, 12, 18]
 dynamicFizzBuzz(20, 4, 6) => [4, 6, 8, 16, 18]
 *******************************************************************************/
-let max=20;
-let num1=5;
-let num2=3;
-function dynamicFizzBuzz(max, num1, num2){
-let array=[];
-
-return array;
+let max = 20;
+let num1 = 5;
+let num2 = 3;
+console.log(dynamicFizzBuzz(max, num1, num2));
+function dynamicFizzBuzz(max, num1, num2) {
+  let array = [];
+  for (let i = 0; i < max; i++) {
+    if(i % num1===0 && i%num2 !==0 || i % num1 !==0 && i%num2 ===0 ){
+      array.push(i);
+    }
+  }
+  return array;
 }
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*************************/
--- week_02/w2d5/A3/problems/5_magic_cipher.js
+++ week_02/w2d5/A3/problems/5_magic_cipher.js
@@ -10,10 +10,21 @@ magicCipher("add me on facebook" , { a : "c", d : "q"}) => "cqq me on fccebook"
 magicCipher("where are you?" , { v : "l", '?' : "!"}) => "where are you!"
 magicCipher("twmce" , { m : "n", t : "d", w : "a"}) => "dance"
 *******************************************************************************/
-
+let sentence="add me on facebook";
+let cipher= { a : "c", d : "q"};
+console.log(magicCipher(sentence, cipher));
 function magicCipher(sentence, cipher){
-  // your code here...
-
+  let newArray=[];
+  let words=sentence.split(" ");
+  for(let i=0;i<words.length;i++){
+    if(cipher[words[i]]===undefined){
+      newArray.push(words[i]);
+    }
+    else {
+      newArray.push(cipher[array[i]]);
+    }
+  }
+  return newArray.join(" ");
 }
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*************************/
