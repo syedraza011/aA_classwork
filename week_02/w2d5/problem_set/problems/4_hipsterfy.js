9dc000522d7e6920664d1876a7eb4940a9af2e6d
Syed Raza <syed.raza011@gmail.com>
2022-10-07 14:29:18 -0400

    Questions done day 5

--- week_02/w2d5/problem_set/problems/4_hipsterfy.js
+++ week_02/w2d5/problem_set/problems/4_hipsterfy.js
@@ -11,9 +11,26 @@ hipsterfy("towel flicker banana"); // => "towl flickr banan"
 hipsterfy("runner anaconda"); // => "runnr anacond"
 hipsterfy("turtle cheeseburger fries"); // => "turtl cheeseburgr fris"
 ***********************************************************************/
+let sentence = "proper tonic panther";
+let vowels = "aeiouAEIOU";
+console.log(hipsterfy(sentence));
 
 function hipsterfy(sentence) {
-
+    let arr = [];
+    let words = sentence.split(" ");
+    for (let i = 0; i < words.length; i++) {
+        let word = words[i];
+       // let chars=word.split("");
+        for (let j = word.length-1; j >=0  ; j--) {
+            let char=word[j];
+            if (vowels.indexOf(char) !== -1) {
+                let newWord = word.slice(0, j) + word.slice(j + 1);
+                arr.push(newWord);
+                break;
+            }
+        }
+    }
+    return arr.join(" ");
 }
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
