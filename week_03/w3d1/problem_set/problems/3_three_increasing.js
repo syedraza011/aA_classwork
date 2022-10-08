5ffedafe4e715a24e36e7cb5754f4fa6a00d5994
Syed Raza <syed.raza011@gmail.com>
2022-10-08 17:59:00 -0400

    Question 3 done

--- week_03/w3d1/problem_set/problems/3_three_increasing.js
+++ week_03/w3d1/problem_set/problems/3_three_increasing.js
@@ -14,7 +14,15 @@ threeIncreasing([7, 2, 4, 5, 2, 1, 6]); // => false
 ***********************************************************************/
 let arr=[3, 2, 11, 12, 13, 2, 4];
 function threeIncreasing(arr) {
-
+for(let i=0;i<arr.length;i++){
+    let first=arr[i];
+    if(first+1===arr[i+1]){
+        if(arr[i+1]+1===arr[i+2]){
+            return true;
+        }
+    }
+}
+return false;
 }
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
