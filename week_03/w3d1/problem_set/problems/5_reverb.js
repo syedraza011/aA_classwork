8580db671810eb7b451f0f285804924db9a90957
Syed Raza <syed.raza011@gmail.com>
2022-10-08 21:15:03 -0400

    Question 5 and home work done

--- week_03/w3d1/problem_set/problems/5_reverb.js
+++ week_03/w3d1/problem_set/problems/5_reverb.js
@@ -9,9 +9,20 @@ reverb('wild'); // => 'wildild'
 reverb('debugged'); // => 'debuggeded'
 reverb('my'); // => 'my'
 ***********************************************************************/
-
+let word='debuggeded';
+let vowels = ['a', 'e', 'i', 'o', 'u','A', 'E', 'I', 'O', 'U'];;
+console.log(reverb(word));
 function reverb(word) {
-
+    for(let i = word.length-1; i >=0; i--){
+        let char=word[i];
+        if(vowels.indexOf(char) > -1){
+        let  afterV=word.slice(i);
+        //console.log(word+afterV)
+        return word+afterV;
+        }
+        
+    }
+    return word;
 }
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
