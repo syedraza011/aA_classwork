ef57877579cf3f73467cd2e6c3e9b30489026490
Syed Raza <syed.raza011@gmail.com>
2022-10-08 22:02:56 -0400

    Question 2 done

--- week_03/w3d1/Practice_asses_4/problems/2_shortest_word.js
+++ week_03/w3d1/Practice_asses_4/problems/2_shortest_word.js
@@ -7,9 +7,18 @@ Examples:
 shortestWord('app academy is cool') => 'is'
 shortestWord('bootcamp prep') => 'prep'
 *******************************************************************************/
-
+let sentence = "app academy is cool";
+console.log(shortestWord(sentence));
 function shortestWord(sentence) {
-  // your code here...
+  let words = sentence.split(" ");
+  let shortest=words[0];
+  for (let i = 0; i < words.length; i++) {
+    let word=words[i];
+    if(shortest.length >word.length){
+      shortest=word;
+    }
+  }
+  return shortest;
 }
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*************************/
