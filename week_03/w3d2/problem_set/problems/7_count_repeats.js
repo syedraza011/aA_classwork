246b6a76eaa3e87d42b7c7941bdada60293ba194
Syed Raza <syed.raza011@gmail.com>
2022-10-09 17:10:07 -0400

    Question 7 done

--- week_03/w3d2/problem_set/problems/7_count_repeats.js
+++ week_03/w3d2/problem_set/problems/7_count_repeats.js
@@ -13,21 +13,34 @@ countRepeats('pops'); //=> 1
 countRepeats('mississippi'); //=> 3
 countRepeats('hellobootcampprep'); //=> 4
 ***********************************************************************/
-let string = "aaaalvin";
-let count={};
-console.log(countRepeats(string));
-function countRepeats(string) {
-    
-    for (let i = 0; i < string.length; i++) {
-        let char = string[i];
-            if(char===undefined){
-                count[char]=1;
-            } else {
-                count[char] +=1;
-            }
-    }
-    return count;
+let str = "hellobootcampprep";
+let count = {};
+console.log(countRepeats(str));
 
+function charCount(str) {
+  var count = {};
+
+  for (var i = 0; i < str.length; i += 1) {
+    var char = str[i];
+
+    if (count[char] === undefined) {
+      count[char] = 1;
+    } else {
+      count[char] += 1;
+    }
+  }
+
+  return count;
+}
+function countRepeats(string) {
+  let count = charCount(string);
+  var numRepeats = 0;
+  for (var char in count) {
+    if (count[char] > 1) {
+      numRepeats += 1;
+    }
+  }
+  return numRepeats;
 }
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
