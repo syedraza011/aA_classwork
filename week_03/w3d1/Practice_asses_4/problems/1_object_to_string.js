367693441ab1fea51db80c314683175afc230447
Syed Raza <syed.raza011@gmail.com>
2022-10-08 21:23:49 -0400

    Week 3 day 1 assesment

---
+++ week_03/w3d1/Practice_asses_4/problems/1_object_to_string.js
@@ -0,0 +1,16 @@
+/*******************************************************************************
+Write a function `objectToString(count)` that takes in a char count object and
+returns a string representing the counts of each character.
+
+Examples:
+
+objectToString({a : 2, b: 4, c: 1}) => 'aabbbbc'
+objectToString({b: 1, o: 2, t: 1}) => 'boot'
+*******************************************************************************/
+
+function objectToString(count) {
+  // your code here...
+}
+
+/**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*************************/
+module.exports = objectToString;
