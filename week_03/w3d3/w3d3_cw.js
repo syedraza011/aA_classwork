b87dd6c26f9235f417b27273b7f367cfa376831a
Syed Raza <syed.raza011@gmail.com>
2022-10-12 18:22:57 -0400

    Class work week 3 day 3

---
+++ week_03/w3d3/w3d3_cw.js
@@ -0,0 +1,7 @@
+// Math 
+Math.floor(3.8) //eounding down so 3 is answer
+Math.PI;
+Math.ceil(3.8) //aproximately 4
+Math.round(3.8) //
+Math.sqrt (9) //3
+Math.abs (-3.2) // 3.2 is absolute value |-3.2|=3.2
