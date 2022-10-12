f265513781817144a7015fa3412e1c5543125a66
Syed Raza <syed.raza011@gmail.com>
2022-10-12 18:26:24 -0400

    Class work

--- week_03/w3d3/w3d3_cw.js
+++ week_03/w3d3/w3d3_cw.js
@@ -5,3 +5,9 @@ Math.ceil(3.8) //aproximately 4
 Math.round(3.8) //
 Math.sqrt (9) //3
 Math.abs (-3.2) // 3.2 is absolute value |-3.2|=3.2
+let radius=4;
+console.log(circleArea(radius));
+function circleArea(radius){
+  let area= Math.PI*Math.sqrt(radius*radius);
+  return Math.floor(area);
+}
