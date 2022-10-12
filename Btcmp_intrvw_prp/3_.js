ab8dcf561f28327111707997d492d9c4848436eb
Syed Raza <syed.raza011@gmail.com>
2022-10-12 13:29:11 -0400

    Question 3 update

---
+++ Btcmp_intrvw_prp/3_.js
@@ -0,0 +1,10 @@
+//Write a function prime that takes in a number and returns a boolean, 
+//indicating whether the number is prime.
+//A prime number is only divisible by 1 and itself.
+// console.log(prime(2))  // true
+// console.log(prime(5))  // true
+// console.log(prime(11)) // true
+// console.log(prime(4))  // false
+// console.log(prime(9))  // false
+// console.log(prime(-5)) // false
+
