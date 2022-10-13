f1015d748ec42ac6855b1c95cfb23371c168987c
Syed Raza <syed.raza011@gmail.com>
2022-10-13 18:25:54 -0400

    New fiolder and lecture sheets added

--- Btcmp_intrvw_prp/adjacentSum.js
+++ Btcmp_intrvw_prp/adjacentSum.js
@@ -1,1 +1,10 @@
-Write a function adjacentSum that takes in an array of numbers and returns a new array containing the sums of adjacent numbers in the original array. See the examples.
+// Write a function adjacentSum that takes in an array of numbers and returns 
+// a new array containing the sums of adjacent numbers in the original array. 
+// See the examples.// [10, 9, 13], because [ 3+7, 7+2, 2+11 ]
+// // [7, 6, 10, 11, 6], because [2+5, 5+1, 1+9, 9+2, 2+4]
+console.log(adjacentSum([3, 7, 2, 11])); 
+console.log(adjacentSum([2, 5, 1, 9, 2, 4])); 
+
+function adjacentSum(numArray) {
+    
+}
