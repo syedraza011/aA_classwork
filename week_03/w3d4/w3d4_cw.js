ea78a4608651fcec669997d362787ef74699c1b4
Syed Raza <syed.raza011@gmail.com>
2022-10-13 18:38:51 -0400

    Class work data modeling

--- week_03/w3d4/w3d4_cw.js
+++ week_03/w3d4/w3d4_cw.js
@@ -1,1 +1,21 @@
 //data modeling
+let student = {
+  name: "xyz",
+  age: "20",
+  height: "5",
+};
+let classOfStudents = {
+  section: "xyz",
+  capacity: "20",
+  lecturer: "Ahmen",
+};
+let car = {
+  make: "Toyota",
+  model: "corolla",
+  year: "2020",
+};
+let parkinglot = {
+  carSpot: "5th fl",
+  carTag: "xbc20",
+  carType: "4-Door",
+};
