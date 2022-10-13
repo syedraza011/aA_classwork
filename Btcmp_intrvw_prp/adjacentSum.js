8744d5e741f631cacbc0ad7b4ccdca397ef7c48f
Syed Raza <syed.raza011@gmail.com>
2022-10-13 19:10:33 -0400

    Question 15 pyramidsum

--- Btcmp_intrvw_prp/adjacentSum.js
+++ Btcmp_intrvw_prp/adjacentSum.js
@@ -1,10 +1,18 @@
-// Write a function adjacentSum that takes in an array of numbers and returns 
-// a new array containing the sums of adjacent numbers in the original array. 
+// Write a function adjacentSum that takes in an array of numbers and returns
+// a new array containing the sums of adjacent numbers in the original array.
 // See the examples.// [10, 9, 13], because [ 3+7, 7+2, 2+11 ]
 // // [7, 6, 10, 11, 6], because [2+5, 5+1, 1+9, 9+2, 2+4]
-console.log(adjacentSum([3, 7, 2, 11])); 
-console.log(adjacentSum([2, 5, 1, 9, 2, 4])); 
+console.log(adjacentSum([3, 7, 2, 11]));
+console.log(adjacentSum([2, 5, 1, 9, 2, 4]));
 
 function adjacentSum(numArray) {
-    
+    let newArray=[];
+    for (let i = 0; i < numArray.length; i++) {
+        if(i<numArray.length-1){
+        let num=numArray[i];
+        let num1=numArray[i+1];
+            newArray.push(num+num1);
+        }
+    }
+    return newArray;
 }
