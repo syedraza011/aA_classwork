e528a0862d0af04b9666f7e5bbd61ad263cfa81c
Syed Raza <syed.raza011@gmail.com>
2022-10-13 09:31:35 -0400

    Question 7 done

--- Btcmp_intrvw_prp/7_.js
+++ Btcmp_intrvw_prp/7_.js
@@ -1,18 +1,23 @@
-//Write a function perfectSquare that takes in a number and 
-//returns a boolean indicating whether it is a perfect square. 
-//A perfect square is a number that results from multiplying a number by itself. 
-//For example, 9 is a perfect square because 3 x 3 = 9, 
+//Write a function perfectSquare that takes in a number and
+//returns a boolean indicating whether it is a perfect square.
+//A perfect square is a number that results from multiplying a number by itself.
+//For example, 9 is a perfect square because 3 x 3 = 9,
 //and 25 is a perfect square because 5 x 5 = 25.
 // console.log(perfectSquare(5));   // false
 // console.log(perfectSquare(12));  // false
 // console.log(perfectSquare(30));  // false
 // console.log(perfectSquare(9));   // true
 // console.log(perfectSquare(25));  // true
-console.log(perfectSquare(5));   // false
-console.log(perfectSquare(12));  // false
-console.log(perfectSquare(30));  // false
-console.log(perfectSquare(9));   // true
-console.log(perfectSquare(25));  // true
+console.log(perfectSquare(5)); // false
+console.log(perfectSquare(12)); // false
+console.log(perfectSquare(30)); // false
+console.log(perfectSquare(9)); // true
+console.log(perfectSquare(25)); // true
 function perfectSquare(num) {
+    for (let i = 0; i <= num; i++) {
+        if (i * i === num) {
+        return true;
+        } 
+    }
+        return false;
 }
-
