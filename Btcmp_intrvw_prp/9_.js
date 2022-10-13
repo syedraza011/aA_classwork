788827af888f88d8da24dd8fa259cde471b1bb89
Syed Raza <syed.raza011@gmail.com>
2022-10-13 09:53:35 -0400

    Quetion 8 done

--- Btcmp_intrvw_prp/9_.js
+++ Btcmp_intrvw_prp/9_.js
@@ -1,8 +1,7 @@
-//A number's summation is the sum of all positive numbers less than 
-//or equal to the number. For example: the summation of 3 is 6 
-//because 1 + 2 + 3 = 6, the summation of 6 is 21 
-//because 1 + 2 + 3 + 4 + 5 + 6 = 21. Write a function 
-//summationSequence that takes in two numbers: start and length.
+//A number's summation is the sum of all positive numbers less than or equal to the number. 
+//For example: the summation of 3 is 6 because 1 + 2 + 3 = 6, 
+//the summation of 6 is 21 because 1 + 2 + 3 + 4 + 5 + 6 = 21. 
+//Write a function summationSequence that takes in two numbers: start and length.
 // The function should return an array containing length total elements. 
 //The first number of the sequence should be the start number.
 //At any point, to generate the next element of the sequence 
