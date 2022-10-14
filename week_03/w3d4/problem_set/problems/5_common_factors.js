aa88c84d1688aef57d00b18f70d5fba29e09b6b9
Syed Raza <syed.raza011@gmail.com>
2022-10-14 13:55:56 -0400

    Question 6 done

--- week_03/w3d4/problem_set/problems/5_common_factors.js
+++ week_03/w3d4/problem_set/problems/5_common_factors.js
@@ -10,12 +10,24 @@ commonFactors(6, 24); // => [ 1, 2, 3, 6 ]
 commonFactors(11, 22); // => [ 1, 11 ]
 commonFactors(45, 60); // => [ 1, 3, 5, 15 ]
 ***********************************************************************/
-commonFactors(12, 50); // => [ 1, 2 ]
-commonFactors(6, 24); // => [ 1, 2, 3, 6 ]
-commonFactors(11, 22); // => [ 1, 11 ]
-commonFactors(45, 60); // => [ 1, 3, 5, 15 ]
+console.log(commonFactors(12, 50)); // => [ 1, 2 ]
+console.log(commonFactors(6, 24)); // => [ 1, 2, 3, 6 ]
+console.log(commonFactors(11, 22)); // => [ 1, 11 ]
+console.log(commonFactors(45, 60)); // => [ 1, 3, 5, 15 ]
 function commonFactors(num1, num2) {
-
+    let cFactor=[];
+    let max=0
+    if(num1<num2){
+        max=num2;
+    } else {
+            max=num1;
+    }
+    for(let i=1;i<max;i++){
+            if(num1 % i ===0 && num2 % i ===0){
+                cFactor.push(i);
+            }
+    }
+return cFactor;
 }
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
--- week_03/w3d4/problem_set/problems/6_common_prime_factors.js
+++ week_03/w3d4/problem_set/problems/6_common_prime_factors.js
@@ -11,10 +11,42 @@ commonPrimeFactors(6, 24); // => [ 2, 3 ]
 commonPrimeFactors(11,22); // => [ 11 ]
 commonPrimeFactors(45, 60); // => [ 3, 5 ]
 ***********************************************************************/
-
+console.log(commonPrimeFactors(12, 50)); // => [ 2 ]
+console.log(commonPrimeFactors(6, 24)); // => [ 2, 3 ]
+console.log(commonPrimeFactors(11,22)); // => [ 11 ]
+console.log(commonPrimeFactors(45, 60)); // => [ 3, 5 ]
 function commonPrimeFactors(num1, num2) {
-
+    let commonfactor=[];
+    let min=0;
+    if(num1>num2){
+        min=num2;
+    }  
+    if(num2>num1){
+        min=num1;
+    }   
+    
+        
+        for(let i=2;i<=min;i++){
+            if(isPrime(i)){
+                if(num1 % i ===0 && num2 % i ===0){
+                commonfactor.push(i);
+                }
+            } 
+        }
+    return commonfactor; 
 }
-
+function isPrime(num){
+    //let newArray=[];
+    if(num<2){
+        return false;;
+    }
+    for(let i=2;i<num;i++){
+        if(num%i ===0){
+        return false;
+        }
+    }
+    return true;
+    //return newArray;
+}
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 module.exports = commonPrimeFactors;
