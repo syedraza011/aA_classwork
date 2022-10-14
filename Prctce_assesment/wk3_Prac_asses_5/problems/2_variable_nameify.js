1c02f2a0d0935ac27d1e5463f0e50a5a02acac84
Syed Raza <syed.raza011@gmail.com>
2022-10-14 16:12:30 -0400

    Question 2 done

--- Prctce_assesment/wk3_Prac_asses_5/problems/2_variable_nameify.js
+++ Prctce_assesment/wk3_Prac_asses_5/problems/2_variable_nameify.js
@@ -9,29 +9,21 @@ variableNameify(['is', 'prime']) => 'isPrime'
 variableNameify(['remove', 'last', 'vowel']) => 'removeLastVowel'
 variableNameify(['MaX', 'VALUE']) => 'maxValue'
 *******************************************************************************/
-console.log(variableNameify(['is', 'prime']))// => 'isPrime'
-console.log(variableNameify(['remove', 'last', 'vowel']))// => 'removeLastVowel'
-console.log(variableNameify(['MaX', 'VALUE']))// => 'maxValue'
+console.log(variableNameify(["is", "prime"])); // => 'isPrime'
+console.log(variableNameify(["remove", "last", "vowel"])); // => 'removeLastVowel'
+console.log(variableNameify(["MaX", "VALUE"])); // => 'maxValue'
 function variableNameify(words) {
-  let name='';
-  for(let i=0;i<words.length;i++){
-    let word=words[i];
-    
-    name=words[0]+firstCap+newWord;
-  }
-  return name;
-}
-function firstCap(word){
-  let capedWord=' ';
-  for(let i=0;i<word.length;i++){
-    let char=word[i];
-    if(word[0]){
-      capedWord +=char.toUpperCase;
+  let variName = " ";
+  for (let i = 0; i < words.length; i++) {
+    let word = words[i];
+    if (i === 0) {
+      variName += word.toLowerCase();
     } else {
-      capedWord +=char
+      let caped = word[0].toUpperCase() + word.slice(1).toLowerCase();
+      variName += caped;
     }
   }
-  return capedWord;
+  return variName;
 }
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*************************/
 module.exports = variableNameify;
