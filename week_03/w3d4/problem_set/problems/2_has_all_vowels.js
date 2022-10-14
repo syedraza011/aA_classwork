a9b646fc82452ccdfc07ced981e5836d12893aee
Syed Raza <syed.raza011@gmail.com>
2022-10-14 11:27:36 -0400

    Question 2 has all vowels

--- week_03/w3d4/problem_set/problems/2_has_all_vowels.js
+++ week_03/w3d4/problem_set/problems/2_has_all_vowels.js
@@ -10,9 +10,14 @@ hasAllVowels('get out of the way, silly'); // => true
 hasAllVowels('bootcamp prep'); // => false
 hasAllVowels('hello world'); // => false
 ***********************************************************************/
-
+console.log(hasAllVowels('have you gone biking?')); // => true
+console.log(hasAllVowels('get out of the way, silly')); // => true
+console.lof(hasAllVowels('bootcamp prep')); // => false
+console.log(hasAllVowels('hello world')); // => false)
 function hasAllVowels(str) {
-
+    for(let i=0;i<str.length;i++){
+        
+    }
 }
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
