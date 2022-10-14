7a97c848c46d0d66e92866269ffff3183e00674f
Syed Raza <syed.raza011@gmail.com>
2022-10-14 15:49:24 -0400

    Question 5 done

--- Prctce_assesment/wk3_Prac_asses_5/problems/5_greater_callback.js
+++ Prctce_assesment/wk3_Prac_asses_5/problems/5_greater_callback.js
@@ -11,7 +11,14 @@ greaterCallback(16.6, Math.floor, Math.ceil) => 17
 console.log(greaterCallback(25, Math.abs, Math.sqrt)); //=> 25
 console.log(greaterCallback(16.6, Math.floor, Math.ceil));// => 17
 function greaterCallback(num, cb1, cb2) {
-  cb1()
+  let result=cb1(num);
+  let result1=cb2(num);
+  if(result>result1){
+    return result;
+  }
+  else {
+    return result1;
+  }
 }
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*************************/
