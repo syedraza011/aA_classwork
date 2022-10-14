928edd44ddb27002dcef086981f71cfd18210cb0
Syed Raza <syed.raza011@gmail.com>
2022-10-14 18:47:47 -0400

    Question 2 done

--- class_assesment/A5/problems/2_snake_to_camel.js
+++ class_assesment/A5/problems/2_snake_to_camel.js
@@ -11,12 +11,19 @@ snakeToCamel('say_hello_world'); // => 'SayHelloWorld'
 snakeToCamel('bootcamp_prep_is_cool'); // => 'BootcampPrepIsCool'
 snakeToCamel('BOOtcamp_PREP_iS_cOol'); // => 'BootcampPrepIsCool'
 *******************************************************************************/
-snakeToCamel('snakes_go_hiss'); // => 'SnakesGoHiss'
-snakeToCamel('say_hello_world'); // => 'SayHelloWorld'
-snakeToCamel('bootcamp_prep_is_cool'); // => 'BootcampPrepIsCool'
-snakeToCamel('BOOtcamp_PREP_iS_cOol'); // => 'BootcampPrepIsCool'
+console.log(snakeToCamel('snakes_go_hiss')); // => 'SnakesGoHiss'
+console.log(snakeToCamel('say_hello_world')); // => 'SayHelloWorld'
+console.log(snakeToCamel('bootcamp_prep_is_cool')); // => 'BootcampPrepIsCool'
+console.log(snakeToCamel('BOOtcamp_PREP_iS_cOol')); // => 'BootcampPrepIsCool'
 function snakeToCamel(str) {
-  
+  let newStr='';
+  let words=str.split('_');
+  for(let i=0;i<words.length;i++){
+    let word=words[i];
+    let newWord= word[0].toUpperCase()+ word.slice(1).toLowerCase();
+    newStr  +=newWord;
+  }
+  return newStr;
 }
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*************************/
