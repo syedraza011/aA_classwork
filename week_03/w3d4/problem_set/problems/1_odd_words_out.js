78d7818fe52a99db63f076f6cf72f42baac2b66d
Syed Raza <syed.raza011@gmail.com>
2022-10-13 23:22:10 -0400

    Question 1 week 3 day 4 done-- odd_words

--- week_03/w3d4/problem_set/problems/1_odd_words_out.js
+++ week_03/w3d4/problem_set/problems/1_odd_words_out.js
@@ -8,9 +8,17 @@ Examples:
 oddWordsOut('go to the store and buy milk'); // => 'go to milk'
 oddWordsOut('what is the answer'); // => 'what is answer'
 ***********************************************************************/
-
+console.log(oddWordsOut('go to the store and buy milk')); // => 'go to milk'
+console.log(oddWordsOut('what is the answer')); // => 'what is answer'
 function oddWordsOut(sentence) {
-
+    let words=sentence.split(" ");
+    let array=[];
+    for(let i=0;i<words.length;i++){
+        if(words[i].length %2 ===0){
+            array.push(words[i]);
+        }
+    }
+    return array.join(" ");
 }
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
