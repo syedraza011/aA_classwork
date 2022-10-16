6bb553027e73f532b4fe93400c6e829655dea013
Syed Raza <syed.raza011@gmail.com>
2022-10-16 14:27:46 -0400

    Question 3 remaining added

---
+++ Btcmp_intrvw_prp/1_ copy.js
@@ -0,0 +1,1 @@
+Write a function handScore that takes in a string representing a hand of cards and returns it's total score. You can assume the letters in the string are only A, K, Q, J. A is worth 4 points, K is 3 points, Q is 2 points, and J is 1 point. The letters of the input string are not necessarily uppercase.
