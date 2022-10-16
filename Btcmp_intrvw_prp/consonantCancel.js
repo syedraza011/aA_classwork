b5777a9df7d713d7af87cbc9da6010cc069576b3
Syed Raza <syed.raza011@gmail.com>
2022-10-16 00:10:32 -0400

    Consonantcancel tough but got it

--- Btcmp_intrvw_prp/consonantCancel.js
+++ Btcmp_intrvw_prp/consonantCancel.js
@@ -4,6 +4,35 @@
 console.log(consonantCancel("down the rabbit hole")); // "own e abbit ole"
 console.log(consonantCancel("writing code is challenging")); // "iting ode is allenging"
 function consonantCancel(sentence) {
-   
+    let array=[];
+    
+    let words=sentence.split(" ");
+    //console.log(words)
+    for(let i=0;i<words.length;i++){
+        let word=words[i];
+        //console.log("Word--Printing--"+word)
+        let newword=removeLetter(word);
+        //console.log("New Word"+newword)
+        array.push(newword);
+    }
+    //console.log(array)
+    return array.join(" ");
 }
 
+function removeLetter(word){
+    //console.log("in function")
+    //console.log("passed functiono"+"  "+word)
+    //let newWord='';
+    let vowels="aeiouAEIOU";
+    for(let i=0;i<word.length;i++){
+        //let char=word[i];
+       // console.log("Char"+" --  "+char)
+        if(vowels.includes(word[i])){
+            return word.slice(i);
+            //console.log("New word"+"  "+newWord)
+            //console.log(newWord)
+        }
+        //console.log("BEfore return "+newWord)
+        //return newWord;
+    }
+}
