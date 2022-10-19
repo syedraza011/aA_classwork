dea147c716109b59ea76909c129b92bb3c2daba6
Syed Raza <syed.raza011@gmail.com>
2022-10-18 23:16:13 -0400

    Question 4 done

--- week_04/w4d1/problem_set/problems/4_smallest_prime.js
+++ week_04/w4d1/problem_set/problems/4_smallest_prime.js
@@ -20,11 +20,11 @@ smallestPrime(arr3); // => null
 var arr4 = [4, 6, 8, 10];
 smallestPrime(arr4); // => null
 ***********************************************************************/
-let arr1 = [ 6, 7, 12, 11, 5, 4 ];
+let arr1 = [6, 7, 12, 11, 5, 4];
 console.log(smallestPrime(arr1)); // => 5
 
 var arr2 = [11, -7, 7, 8, 6, 10];
-//console.log(smallestPrime(arr2)); // => 7
+console.log(smallestPrime(arr2)); // => 7
 
 var arr3 = [];
 //console.log(smallestPrime(arr3)); // => null
@@ -32,32 +32,74 @@ var arr3 = [];
 var arr4 = [4, 6, 8, 10];
 //console.log(smallestPrime(arr4)); // => null
 function smallestPrime(array) {
-    let smallest=55555;
-    if(array.length<1){
-        return null;
+  let smallest = null;
+//   if (array.length < 1) {
+//     return null;
+//   }
+  for (let i = 0; i < array.length; i++) {
+    let num = array[i];
+    if (isPrime(num) && (num < smallest || smallest === null)) {
+      smallest = num;
+      console.log("Smallest" + smallest);
     }
-    for(let i=0;i<array.length;i++){
-
-        if(isPrime(array[i])){
-           if(smallest>array[i]) 
-           smallest=array[i]
-            console.log("Smallest"+smallest)
-        }
-    }
-    return smallest;
-}
-function isPrime(num){
-    if(num<2){
-        return false;
-    }
-    for(let i=0;i<num;i++){
-        if(num%i===0){
-            return false;
-        }
-        
-    }
-    return true;
+  }
+  return smallest;
 }
 
+//function smallestPrime(array) {
+    //var smallest = null;
+  
+    //for (var i = 0; i < array.length; i += 1) {
+//      // var num = array[i];
+  
+//       if (isPrime(num) && (num < smallest || smallest === null)) {
+//         smallest = num;
+//       }
+//     }
+  
+//     return smallest;
+//   }
+
+
+
+function isPrime(num) {
+  if (num < 2) {
+    return false;
+  }
+  for (let i = 2; i <=  Math.sqrt(num); i++) {
+    if (num % i === 0) {
+      return false;
+    }
+  }
+  return true;
+}
+
+function smallestPrime(array) {
+  var smallest = null;
+
+  for (var i = 0; i < array.length; i += 1) {
+    var num = array[i];
+
+    if (isPrime(num) && (num < smallest || smallest === null)) {
+      smallest = num;
+    }
+  }
+
+  return smallest;
+}
+
+//function isPrime(num) {
+    // if (num < 2) {
+    //   return false;
+    // }
+  
+//     for (var i = 2; i  <= Math.sqrt(num); i += 1) {
+//       if (num % i === 0){
+//         return false
+//       }
+//     }
+  
+//     return true;
+//   }
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 module.exports = smallestPrime;
