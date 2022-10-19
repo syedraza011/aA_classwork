212aae7a2e544a0f10d9ea6c10e1604063641a56
Syed Raza <syed.raza011@gmail.com>
2022-10-19 00:56:59 -0400

    Question 1_safe speed change

--- week_04/w4d2/problem_set/problems/1_safe_speed_change.js
+++ week_04/w4d2/problem_set/problems/1_safe_speed_change.js
@@ -9,9 +9,20 @@ safeSpeedChange([3, 3, 2, 6, 8, 7]); //=> true
 safeSpeedChange([3, 3, 2, 6, 12, 10]); // => false
 safeSpeedChange([8, 10, 4, 3, 2]); // => false
 ***********************************************************************/
-
+console.log(safeSpeedChange([3, 3, 2, 6, 8, 7])); //=> true
+console.log(safeSpeedChange([3, 3, 2, 6, 12, 10])); // => false
+console.log(safeSpeedChange([8, 10, 4, 3, 2])); // => false
 function safeSpeedChange(speeds) {
-
+    //let flag=false;
+for(let i=0;i<speeds.length-1;i++){
+    let num1=speeds[i];
+    let num2=speeds[i+1];
+    if(Math.abs(num2-num1)>5){
+        return false;
+    }
+    
+}
+return true;
 }
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
