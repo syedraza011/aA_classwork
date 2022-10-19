749e1abe04f1b8d8f279468a2142ca01dd2c3bcf
Syed Raza <syed.raza011@gmail.com>
2022-10-19 00:36:18 -0400

    Questions done

--- week_04/w4d1/problem_set/problems/5_can_construct.js
+++ week_04/w4d1/problem_set/problems/5_can_construct.js
@@ -11,13 +11,34 @@ canConstruct("a", "aa");              // => true
 canConstruct("ababc", "dbaccab");     // => true
 canConstruct("aabbc", "aaaabbbdef");  // => false
 ***********************************************************************/
-console.log(canConstruct("a", "b"));               // => false
-console.log(canConstruct("a", "aa"));              // => true
+//console.log(canConstruct("a", "b"));               // => false
+//console.log(canConstruct("a", "aa"));              // => true
 console.log(canConstruct("ababc", "dbaccab"));     // => true
 console.log(canConstruct("aabbc", "aaaabbbdef"));  // => false
 function canConstruct(ransomNote, magazine) {
-
+    let magCount=countEle(magazine);
+    let ranCount=countEle(ransomNote);
+    console.log(magCount);
+    console.log(ranCount);
+    for (let char in ranCount) {
+        const numLetters = ranCount[char];
+        if (magCount[char] === undefined || magCount[char] < numLetters) {
+            return false;
+        }
+    }
+return true;
 }
-
+function countEle(string){
+    let count={};
+    for(let i=0;i<string.length;i++){
+           if(string[i] in count){ 
+            count[string[i]]++;
+        }
+        else {
+            count[string[i]]=1;
+        }
+    }
+    return count;
+}
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 module.exports = canConstruct;
