14390db7622e86c8cf6ca9c383f2638daa8d5254
Syed Raza <syed.raza011@gmail.com>
2022-10-20 04:51:47 -0400

    Question tally_count

--- week_04/w4d3/problem_set/problems/6_tally_count.js
+++ week_04/w4d3/problem_set/problems/6_tally_count.js
@@ -11,10 +11,24 @@ tallyCount(arr1); // => { cat: 'III', dog: 'II', fish: 'I', squirrel: 'I' }
 var arr2 = ['x', 'x', 'y', 'z', 'y', 'x']
 tallyCount(arr2); // => { x: 'III', y: 'II', z: 'I' }
 ***********************************************************************/
+let array = ['cat', 'dog', 'cat', 'cat', 'fish', 'dog', 'squirrel']
+console.log(tallyCount(array)); // => { cat: 'III', dog: 'II', fish: 'I', squirrel: 'I' }
 
-function tallyCount(arr) {
-
+// let arr2 = ['x', 'x', 'y', 'z', 'y', 'x']
+// tallyCount(arr2); // => { x: 'III', y: 'II', z: 'I' }
+function tallyCount(array) {
+    let tally={};
+    for(let i=0;i<array.length;i++){
+        let char=array[i];
+        if(tally[char]===undefined){
+            tally[char]='I';
+        } else {
+            tally[char] +='I'
+        }
+    }
+    return tally;
 }
+
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 module.exports = tallyCount;
