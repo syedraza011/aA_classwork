16c61ee2d1b3771994d3462dcf9ac6b32078f05e
Syed Raza <syed.raza011@gmail.com>
2022-10-20 17:22:27 -0400

    Question split half array

--- week_03/w3d5/problem_set/problems/2_split_half_array.js
+++ week_03/w3d5/problem_set/problems/2_split_half_array.js
@@ -12,9 +12,18 @@ splitHalfArray([1, 2, 3, 4, 5]);
 splitHalfArray(['a', 'b', 'c', 'd', 'e', 'f']);
 // => [ [ 'a', 'b', 'c' ], [ 'd', 'e', 'f' ] ]
 ***********************************************************************/
+console.log(splitHalfArray([1, 2, 3, 4, 5]));
+// => [ [ 1, 2 ], [ 4, 5 ] ]
 
+console.log(splitHalfArray("a", "b", "c", "d", "e", "f"));
+// => [ [ 'a', 'b', 'c' ], [ 'd', 'e', 'f' ] ]
 function splitHalfArray(array) {
-
+  midIndex = Math.floor(array.length / 2);
+  if (array.length % 2 === 0) {
+    return [array.slice(0, midIndex), array.slice(midIndex)];
+  } else {
+    return [array.slice(0, midIndex), array.slice(midIndex + 1)];
+  }
 }
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
