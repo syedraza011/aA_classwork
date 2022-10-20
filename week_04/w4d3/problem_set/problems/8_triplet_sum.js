b4ebd3c4d87ba1a417b39a7a1c1a60e3b129aad7
Syed Raza <syed.raza011@gmail.com>
2022-10-20 11:12:08 -0400

    Question triplet_ SUm

--- week_04/w4d3/problem_set/problems/8_triplet_sum.js
+++ week_04/w4d3/problem_set/problems/8_triplet_sum.js
@@ -12,9 +12,25 @@ tripletSum(arr1, 8); // => [ [ 1, 3, 4 ], [ 1, 5, 2 ] ]
 tripletSum(arr1, 10); // => [ [ 1, 5, 4 ], [ 3, 5, 2 ] ]
 tripletSum(arr1, 11); // => [ [ 5, 2, 4 ] ]
 ***********************************************************************/
-
+let arr1 = [1, 3, 5, 2, 4];
+console.log(tripletSum(arr1, 8)); // => [ [ 1, 3, 4 ], [ 1, 5, 2 ] ]
+console.log(tripletSum(arr1, 10)); // => [ [ 1, 5, 4 ], [ 3, 5, 2 ] ]
+console.log(tripletSum(arr1, 11)); // => [ [ 5, 2, 4 ] ]
 function tripletSum(array, sum) {
-
+    let newArray=[];
+    for(let i=0;i<array.length;i++){
+        let num1=array[i];
+        for(let j=i+1;j<array.length;j++){
+            let num2=array[j];
+            for(let k=j+1;k<array.length;k++){
+                let num3=array[k];
+                if(num1+num2+num3===sum){
+                    newArray.push([num1,num2,num3]);
+                }
+            }
+        }
+    }
+    return newArray;
 }
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
