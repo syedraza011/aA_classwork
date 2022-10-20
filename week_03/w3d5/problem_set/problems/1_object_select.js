db4a6ac1bd43ac6596cba92ac656a582b6ba22a5
Syed Raza <syed.raza011@gmail.com>
2022-10-20 17:01:12 -0400

    Question 1_object_selection

--- week_03/w3d5/problem_set/problems/1_object_select.js
+++ week_03/w3d5/problem_set/problems/1_object_select.js
@@ -23,9 +23,27 @@ matchingPairs; //=> {
   three: "three"
 }
 ***********************************************************************/
+var obj = {
+  one: "one",
+  two: "something else",
+  three: "three",
+  four: "another thing."
+};
 
+function isEqual(a, b) {
+  
+  return a === b;
+}
+//console.log(objectSelect(obj, callback));
 function objectSelect(obj, callback) {
-
+  let result={};
+for(let key in obj){
+    let val=obj[key]
+    if(callback(key,val)){
+      result[key]=val;
+    }
+}
+return result;
 }
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
--- week_03/w3d5/problem_set/problems/3_three_unique_vowels.js
+++ week_03/w3d5/problem_set/problems/3_three_unique_vowels.js
@@ -12,9 +12,16 @@ threeUniqueVowels('bootcamp'); // => false
 threeUniqueVowels('dog'); // => false
 threeUniqueVowels('go home'); // => false
 ***********************************************************************/
-
+console.log(threeUniqueVowels('delicious')); // => true
+console.log(threeUniqueVowels('bootcamp prep')); // => true
+console.log(threeUniqueVowels('bootcamp')); // => false
+console.log(threeUniqueVowels('dog')); // => false
+console.log(threeUniqueVowels('go home')); // => false
 function threeUniqueVowels(string) {
+    let vowels='AEIOUaeiou';
+for(let i=0;i<string.length;i++){
 
+}
 }
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
