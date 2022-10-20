bd8662a114686c33219137fdfc57710a235617e4
Syed Raza <syed.raza011@gmail.com>
2022-10-19 20:14:30 -0400

    Question 3_Alternate case

--- week_04/w4d3/problem_set/problems/3_alternate_case.js
+++ week_04/w4d3/problem_set/problems/3_alternate_case.js
@@ -8,9 +8,21 @@ alternateCase('BOOTCAMPPREP'); // => 'BoOtCaMpPrEp'
 alternateCase('bOotCamPpREP'); // => 'BoOtCaMpPrEp'
 alternateCase('hello'); // => 'HeLlO'
 ***********************************************************************/
-
+console.log(alternateCase('BOOTCAMPPREP')); // => 'BoOtCaMpPrEp'
+console.log(alternateCase('bOotCamPpREP')); // => 'BoOtCaMpPrEp'
+console.log(alternateCase('hello')); // => 'HeLlO'
 function alternateCase(str) {
-
+    let words=str.toLowerCase();
+    let string='';
+for(let i=0;i<words.length;i++){
+    let char=words[i];
+    if(i%2 ===0){
+        string +=char.toUpperCase();
+    } else {
+            string +=char;
+    }
+}
+return string;
 }
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
