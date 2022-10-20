fb6ef84bf14f0324520ffcc8e7cfdcba1ba02713
Syed Raza <syed.raza011@gmail.com>
2022-10-19 20:06:50 -0400

    Question 2_remove nth letter done

--- week_04/w4d3/problem_set/problems/2_remove_nth_letter.js
+++ week_04/w4d3/problem_set/problems/2_remove_nth_letter.js
@@ -10,9 +10,22 @@ removeNthLetter('bootcamp', 6); // => 'bootcap'
 removeNthLetter('bootcamp', 4); // => 'bootamp'
 removeNthLetter('computer', 3); // => 'comuter'
 ***********************************************************************/
-
+console.log(removeNthLetter('bootcamp', 0)); // => 'ootcamp'
+console.log(removeNthLetter('bootcamp', 6)); // => 'bootcap'
+console.log(removeNthLetter('bootcamp', 4)); // => 'bootamp'
+console.log(removeNthLetter('computer', 3)); // => 'comuter'
 function removeNthLetter(word, n) {
-
+    let newWord='';
+    for(let i=0;i<word.length;i++){
+        if(i===n){
+            
+            newWord +='';
+        }
+        else {
+            newWord +=word[i];
+        }
+    }
+return newWord;
 }
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
