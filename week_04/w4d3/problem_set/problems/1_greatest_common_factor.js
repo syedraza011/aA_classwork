d9d82ec975a83e3d298c63908667da64cdf08827
Syed Raza <syed.raza011@gmail.com>
2022-10-19 20:00:18 -0400

    Question 1_greatest Done

--- week_04/w4d3/problem_set/problems/1_greatest_common_factor.js
+++ week_04/w4d3/problem_set/problems/1_greatest_common_factor.js
@@ -12,10 +12,24 @@ greatestCommonFactor(4, 7); // => 1
 greatestCommonFactor(4, 8); // => 4
 greatestCommonFactor(45, 30); // => 15
 ***********************************************************************/
-
+console.log(greatestCommonFactor(6, 10)); // => 2
+console.log(greatestCommonFactor(10, 15)); // => 5
+console.log(greatestCommonFactor(4, 7)); // => 1
+console.log(greatestCommonFactor(4, 8)); // => 4
+console.log(greatestCommonFactor(45, 30)); // => 15
 function greatestCommonFactor(num1, num2) {
-
+  let lDivisor = 0;
+  let num = num2;
+  if (num1 < num2) {
+    let num = num1;
+  } 
+  for (let i = num; i => num; i--) {
+    if(num1 % i===0 && num2 %i===0){
+            lDivisor = i;
+            return lDivisor;
+    }
 }
 
+}
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 module.exports = greatestCommonFactor;
