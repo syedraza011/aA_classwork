49b6974a4b7612b61af5fbc67e319295e4903476
Syed Raza <syed.raza011@gmail.com>
2022-10-21 12:09:29 -0400

    Question reverse string done

--- class_assesment/final_eval/problems/2_reverse_string.js
+++ class_assesment/final_eval/problems/2_reverse_string.js
@@ -9,9 +9,22 @@ reverseString('hello world'); // => 'dlrow olleh'
 
 Difficulty: Easy
 *************************************************************************************/
-
+console.log(reverseString('taco'));        // => 'ocat'
+console.log(reverseString('hello world')); // => 'dlrow olleh'
 function reverseString(str) {
-
+    let array=[];
+    let words=str.split(" ");
+for(let i=words.length-1;i>=0;i--){
+    let newWord='';
+    let word=words[i]
+    for(let j=word.length-1;j>=0;j--){
+        newWord +=word[j];
+        
+    }
+    array.push(newWord);
+ 
+}
+return array.join(" ");
 }
 
 /******************** DO NOT MODIFY ANYTHING UNDER THIS LINE *************************/
