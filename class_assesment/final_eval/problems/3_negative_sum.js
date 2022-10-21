a6bae1f614121ada9ea7d7d62533814ed6a43cd0
Syed Raza <syed.raza011@gmail.com>
2022-10-21 12:15:41 -0400

    Question 3 SUm negatives done

--- class_assesment/final_eval/problems/3_negative_sum.js
+++ class_assesment/final_eval/problems/3_negative_sum.js
@@ -9,9 +9,17 @@ negativeSum([2, 1, 7]);           // => 0
 
 Difficulty: Easy
 *************************************************************************************/
-
+console.log(negativeSum([-3, 12, -5, -2, 3])); // => -10
 function negativeSum(arr) {
-
+    let neg=0;
+for(let i=0;i<arr.length;i++){
+    let num=arr[i];
+    if(num<0){
+        neg +=num;
+    }
+    
+}
+return neg;
 }
 
 /******************** DO NOT MODIFY ANYTHING UNDER THIS LINE *************************/
