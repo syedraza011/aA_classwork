941c5f5d138f5bfeef0959ffe7061cfe8c3344a8
Syed Raza <syed.raza011@gmail.com>
2022-10-22 11:44:31 -0400

    Question are Co-prime

--- class_assesment/final_eval/problems/6_are_coprime.js
+++ class_assesment/final_eval/problems/6_are_coprime.js
@@ -22,15 +22,17 @@ console.log(areCoprime(15, 14)); // => true
 console.log(areCoprime(14, 21)); // => false
 console.log(areCoprime(15, 6)); // => false
 console.log(areCoprime(15, 5)); // => false
-function areCoprime(num1, num2) {}
-function isPrime(num) {
-  if (num < 2) {
-    return false;
+function areCoprime(num1, num2) {
+  let smaller = num2;
+  if (num1 < smaller) {
+    smaller = num1;
   }
-  for (let i = 2; i < num; i++) {
-    if (num % i !== 0) {
+  for (let i = 2; i <= smaller; i++) {
+    if (num1 % i === 0 && num2 % i === 0) {
+      return false;
     }
   }
+  return true;
 }
 /******************** DO NOT MODIFY ANYTHING UNDER THIS LINE *************************/
 
