82804ea71b72986abac88962d0293d50670bc9da
Syed Raza <syed.raza011@gmail.com>
2022-10-23 13:45:08 -0400

    Question 1_censor DOne

--- week_04/w4d5/problem_set/problems/1_censor.js
+++ week_04/w4d5/problem_set/problems/1_censor.js
@@ -9,10 +9,26 @@ result1; //=> "D*rn you Harold you son of a g*n"
 var result2 = censor("Schnikeys I dont give a diddly squat", ["schnikeys", "diddly", "squat"]);
 result2; //=> "Schn*k*ys I dont give a d*ddly sq**t"
 ***********************************************************************/
+let result1 = censor("Darn you Harold you son of a gun", ["darn", "gun"]);
+result1; //=> "D*rn you Harold you son of a g*n"
 
+let result2 = censor("Schnikeys I dont give a diddly squat", ["schnikeys", "diddly", "squat"]);
+console.log(result2); //=> "Schn*k*ys I dont give a d*ddly sq**t"
 function censor(sentence, curseWords) {
 
 }
-
+function removeVowels(word){
+    let vowels="AEIOUaeiou"
+    let newWord='';
+    for(let i=0;i<word.length;i++){
+        let char=word[i];
+        if(vowels.includes(char)){
+            newWord='*';
+        }
+        else {
+            
+        }
+    }
+}
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 module.exports = censor;
