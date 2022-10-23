fe0613ae2ca0d4e782a5e1653726535e3536cae8
Syed Raza <syed.raza011@gmail.com>
2022-10-23 17:11:01 -0400

    Question isogram done

--- week_04/w4d5/problem_set/problems/3_isogram_matcher.js
+++ week_04/w4d5/problem_set/problems/3_isogram_matcher.js
@@ -12,9 +12,29 @@ isogramMatcher("home", "dome"); //=> [3, 0]
 isogramMatcher("ultrasonic", "ostracized"); //=> [3, 4]
 isogramMatcher("unpredictably", "hydromagnetic"); //=> [1, 8]
 ***********************************************************************/
-
+console.log(isogramMatcher("an", "at")); //=> [1, 0]
+console.log(isogramMatcher("or", "go")); //=> [0, 1]
+console.log(isogramMatcher("cat", "tap")); //=> [1, 1]
 function isogramMatcher(string1, string2) {
+    let iCounter=0;
+    let pCounter=0;
+let arr=[];
+for(let i=0;i<string1.length;i++){
+    let char=string1[i];
+    //console.log("CHAR---->  "+char)
+    if(char===string2[i]){
+        iCounter++
+        //console.log("includes---->  "+iCounter)
+    } else if(string2.indexOf(char)>-1){
+        pCounter++;
+        //console.log("position---->  "+pCounter)
+    } 
+    // console.log("included-->"+iCounter)
+    // console.log("Not___included-->"+pCounter)
+    
+}
 
+return [iCounter,pCounter];
 }
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
