0cce10f67ad65fdd2a4c4597c49dcfdcb682d517
Syed Raza <syed.raza011@gmail.com>
2022-10-23 17:28:32 -0400

    Question second largest

--- week_04/w4d5/problem_set/problems/4_second_largest.js
+++ week_04/w4d5/problem_set/problems/4_second_largest.js
@@ -7,13 +7,28 @@ largest number, the function should return -1.
 Examples:
 
 secondLargest([3, 1, 5, 4, 7]) => 5;
-secondLargest([10, 7]) => 7;
+console.log(secondLargest([10, 7])); //=> 7;
 secondLargest([10]) => -1;
 secondLargest([]) => -1;
 ***********************************************************************/
-
+console.log(secondLargest([10, 7])); //=> 7;
+console.log(secondLargest([3, 1, 5, 4, 7]))// => 5;
 function secondLargest(array) {
-
+    
+    let max=-1;
+    let secondMax=-1;
+for(let i=0;i<array.length;i++){
+    let num=array[i];
+    if(max<num){
+        secondMax=max;
+        max=num;
+    } else if(num>secondMax){
+        secondMax = num
+    }
+    //console.log("Max---> "+max)
+    //console.log("Prev Max---> "+secondMax)
+}
+return secondMax
 }
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
