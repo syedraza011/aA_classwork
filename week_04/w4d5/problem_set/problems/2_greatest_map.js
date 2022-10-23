33f493d0757b122956a7f23d612986aabc96e79d
Syed Raza <syed.raza011@gmail.com>
2022-10-23 16:43:25 -0400

    Question greatest maped

--- week_04/w4d5/problem_set/problems/2_greatest_map.js
+++ week_04/w4d5/problem_set/problems/2_greatest_map.js
@@ -24,12 +24,22 @@ Hint:
 (squareRootRounder(5); //=> 2
 (squareRootRounder(10); //=> 3
 ***********************************************************************/
-
-console.log(squareRootRounder(1)); //=> 1
-console.log(squareRootRounder(5); //=> 2
-console.log(squareRootRounder(10); //=> 3
+let array = [1, 5, 10];
+// console.log(squareRootRounder(1)); //=> 1
+// console.log(squareRootRounder(5)); //=> 2
+// console.log(squareRootRounder(10)); //=> 3
 function greatestMap(array, cb1, cb2) {
-
+  let newArray=[];
+for (let i=0;i< array.length;i++){
+ let result=cb1(array[i],i,array);
+ let result1=cb2(array[i],i,array);
+ if(result>result1){
+  newArray.push(result);
+ } else {
+  newArray.push(result1);
+ }
+}
+return newArray;
 }
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
