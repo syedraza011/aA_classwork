28512c52cd7611be982ce87d7ef74dfcf1c12ad6
Syed Raza <syed.raza011@gmail.com>
2022-10-23 20:45:53 -0400

    Question Binary search done

--- Algorithms/2_binary_search..js
+++ Algorithms/2_binary_search..js
@@ -1,18 +1,22 @@
 //Binary Search
 let arr=[1,2,3,4,5,6,7,8]
-let target=5;
-BinarySearch(arr,target);
+let target=8;
+console.log(BinarySearch(arr,target));
 function BinarySearch(arr,target){
   let mid=Math.floor(arr.length/2);
-  let left=arr[mid-1];
-  let right=arr[mid+1];
-  if(mid===target){
+  console.log("MID---> "+mid)
+  let left=arr.slice(0,mid);
+  console.log("left---> "+left)
+  let right=arr.slice(mid+1);
+  console.log("Right---> "+right)
+  if(arr[mid]===target){
     return mid;
 }
-else if(mid < left){
+else if(target < arr[mid]){
   return BinarySearch(left,target)
 }
 else {
-  return BinarySearch(right,target)
+  let rest= BinarySearch(right,target)
+  return rest===-1 ? -1: mid+1+rest
 }
 }
