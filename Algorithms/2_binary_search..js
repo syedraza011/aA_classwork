8fda69e3343f2357cc93ced53b6bf57af019fc39
Syed Raza <syed.raza011@gmail.com>
2022-10-23 20:53:39 -0400

    Radix Sort

--- Algorithms/2_binary_search..js
+++ Algorithms/2_binary_search..js
@@ -1,22 +1,23 @@
 //Binary Search
-let arr=[1,2,3,4,5,6,7,8]
-let target=8;
-console.log(BinarySearch(arr,target));
-function BinarySearch(arr,target){
-  let mid=Math.floor(arr.length/2);
-  console.log("MID---> "+mid)
-  let left=arr.slice(0,mid);
-  console.log("left---> "+left)
-  let right=arr.slice(mid+1);
-  console.log("Right---> "+right)
-  if(arr[mid]===target){
+let arr = [10, 20, 30, 40, 50, 60, 70, 80];
+let target = 20;
+console.log(BinarySearch(arr, target));
+if(arr.length===0){
+  return -1;
+}
+function BinarySearch(arr, target) {
+  let mid = Math.floor(arr.length / 2);
+  console.log("MID---> " + mid);
+  let left = arr.slice(0, mid);
+  console.log("left---> " + left);
+  let right = arr.slice(mid + 1);
+  console.log("Right---> " + right);
+  if (arr[mid] === target) {
     return mid;
-}
-else if(target < arr[mid]){
-  return BinarySearch(left,target)
-}
-else {
-  let rest= BinarySearch(right,target)
-  return rest===-1 ? -1: mid+1+rest
-}
+  } else if (target < arr[mid]) {
+    return BinarySearch(left, target);
+  } else {
+    let rest = BinarySearch(right, target);
+    return rest === -1 ? -1 : mid + 1 + rest;
+  }
 }
