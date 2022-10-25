72592c698fa2f29c9b61aa3e2e6f0db98af7666d
Syed Raza <syed.raza011@gmail.com>
2022-10-25 12:16:09 -0400

    Question substring added

--- Btcmp_intrvw_prp/sameStringArrays.js
+++ Btcmp_intrvw_prp/sameStringArrays.js
@@ -1,12 +1,8 @@
-// Given two string arrays word1 and word2, return true if the two arrays represent the same string, and false otherwise.
-
-// A string is represented by an array if the array elements concatenated in order forms the string.
-
- 
-
-// Example 1:
-
-// Input: word1 = ["ab", "c"], word2 = ["a", "bc"]
+// Given two string arrays word1 and word2, 
+//return true if the two arrays represent the same string, and false otherwise.
+// A string is represented by an array if the array elements concatenated in order 
+//forms the string.
+// Example 1:// Input: word1 = ["ab", "c"], word2 = ["a", "bc"]
 // Output: true
 // Explanation:
 // word1 represents string "ab" + "c" -> "abc"
@@ -21,14 +17,21 @@
 // Input: word1  = ["abc", "d", "defg"], word2 = ["abcddefg"]
 // Output: true
 
-
-
-var arrayStringsAreEqual = function(word1, word2) {
-  for(let i=0;i<word1;i++){
-      if(word1[i]!==word2[i]){
-          return false;
-          
+let word1 = ["a", "bc"];
+let word2 = ["ab", "c"];
+console.log(arrayStringsAreEqual(word1, word2));
+function arrayStringsAreEqual(word1, word2) {
+  let nWord1=word1.join(" ");
+  let nWord2=word2.join(" ");
+  for(let i=0;i<nWord1.length;i++){
+    let aWord=nWord1[i];
+    console.log("a Word===--->> "+aWord);
+    for(let j=0;j<nWord2.length;j++){
+      if(aWord !==word2[i]){
+       
       }
+      return false;
+    }
   }
   return true;
 };
