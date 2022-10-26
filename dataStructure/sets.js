f7c9fa12a4cc563e9d95ef85aec8ffe9a388ab07
Syed Raza <syed.raza011@gmail.com>
2022-10-26 17:43:01 -0400

    Question set updated

--- dataStructure/sets.js
+++ dataStructure/sets.js
@@ -1,1 +1,12 @@
 //sets
+const myset= new Set();
+//add value
+myset.add(60);
+myset.add({name: 'dom',
+  age: 20
+});
+myset.add(79.7);
+console.log(myset);
+console.log("Printing size of set-->"+myset.size);
+
+//
