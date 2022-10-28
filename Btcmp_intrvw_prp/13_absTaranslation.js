787eb991f1c9cd758b90da76661703dbab80ca6d
Syed Raza <syed.raza011@gmail.com>
2022-10-28 16:15:48 -0400

    Question 13_abs translated Updated

--- Btcmp_intrvw_prp/13_absTaranslation.js
+++ Btcmp_intrvw_prp/13_absTaranslation.js
@@ -11,3 +11,28 @@
 // // aba_translate(“Everyone can code”) #=> “Ebeveberyobonebe caban cobodebe”
 // // aba_translate(“Africa is Africa in German”) 
 // //#=> “Abafribicaba ibis Abafribicaba ibin Gebermaban”
+console.log(aba_translate("Africa is Africa in German"));
+console.log(aba_translate("Cats and dogs"));
+function aba_translate(sentence){
+    let arr=[];
+    let words=sentence.split(" ");
+    for (let i=0;i<words.length;i++){
+        let word=words[i];
+        arr.push(translated(word));
+    }
+    return arr.join(" ");
+}
+function translated(word){
+    let newWord=''; //c //cabac
+    vowels="AEIOUaeiou";
+    for(let i=0;i<word.length;i++){ //i=0 word=cat so  //i=1 
+        let char=word[i]; // c //a
+        if(vowels.includes(char)){
+            newWord +=char+'b'+char; //c //caba
+        }
+        else {
+            newWord +=char; // c added here 
+        }
+    }
+    return newWord;
+}
