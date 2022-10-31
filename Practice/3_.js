0cbb15f4f6aa8b37115c56bb3c9799ba763400f3
Syed Raza <syed.raza011@gmail.com>
2022-10-31 10:35:36 -0400

    Qustion udpate

---
+++ Practice/3_.js
@@ -0,0 +1,11 @@
+function lastLetter(word) {
+    //for(let i=word.length-1;i>=0;i--){
+    return word[word.length-1];
+    //}
+  }
+  
+  // Uncomment the lines below to test your code
+  console.log(lastLetter("hello")); //=> "o"
+  console.log(lastLetter("goodbye!")); //=> "!"
+  console.log(lastLetter("ZeltoiD")); //=> "D"
+  console.log(lastLetter("I love Javascript")); //=> "t"
