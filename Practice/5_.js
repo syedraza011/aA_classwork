5077458d1fd597fddc2c21e48768e00bc3e61032
Syed Raza <syed.raza011@gmail.com>
2022-11-01 12:13:57 -0400

    Question done

--- Practice/5_.js
+++ Practice/5_.js
@@ -1,3 +1,7 @@
+
+/*Examine the code given to you. Determine what arguments must be passed 
+into isX1 and isX2 respectively to obtain an output of true for both, 
+and pass in the arguments accordingly. */
 const x = 3;
 
 function isX1(num) {
