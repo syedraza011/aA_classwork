edc3da1d6d0729ff92fcc778f5bff4d59bf0788b
Syed Raza <syed.raza011@gmail.com>
2022-11-02 19:20:37 -0400

    Question updated

---
+++ Practice/doubleLetterCount.js
@@ -0,0 +1,7 @@
+// Write a function doubleLetterCount that takes in a string and 
+// returns the number of times that the same letter repeats twice in a row.
+console.log(doubleLetterCount("the jeep rolled down the hill"));  // 3
+console.log(doubleLetterCount("bootcamp")); // 1
+function doubleLetterCount (str){
+    
+}
