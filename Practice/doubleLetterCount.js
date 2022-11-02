74594b2e7487144f985a9c4342fbf6a5a2bd6ce1
Syed Raza <syed.raza011@gmail.com>
2022-11-02 19:25:21 -0400

    Question practiced

--- Practice/doubleLetterCount.js
+++ Practice/doubleLetterCount.js
@@ -3,5 +3,22 @@
 console.log(doubleLetterCount("the jeep rolled down the hill"));  // 3
 console.log(doubleLetterCount("bootcamp")); // 1
 function doubleLetterCount (str){
-    
+let count=0;
+    let words=str.split(" ");
+    for (let i=0;i<words.length;i++){
+        let word=words[i];
+       count += letterCount(word);
+    }
+    return count;
+}
+function letterCount(word){
+    let counter=0;
+    for(let i=0;i<word.length;i++){
+        let char1=word[i];
+        let char2=word[i+1];
+        if(char1===char2){
+            counter++;
+        }
+    }
+    return counter;
 }
