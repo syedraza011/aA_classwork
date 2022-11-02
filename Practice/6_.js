b3b13d9b9bdd11af257d9034c54158191dc67245
Syed Raza <syed.raza011@gmail.com>
2022-11-02 19:27:09 -0400

    Question practiced

---
+++ Practice/6_.js
@@ -0,0 +1,7 @@
+// Write a function pyramidSum that takes in an array of numbers representing 
+// the base of a pyramid. The function should return a 2D array representing 
+// a complete pyramid with the given base. 
+// To construct a level of the pyramid, we take the sum of adjacent
+//  elements of the level below.
+console.log(pyramidSum([1, 4, 6])); // [[15], [5, 10], [1, 4, 6]]
+console.log(pyramidSum([3, 7, 2, 11])); // [[41], [19, 22], [10, 9, 13], [3, 7, 2, 11]]
