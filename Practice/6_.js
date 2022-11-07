c41f1a1e27781ed382ee923ea09915823ee09101
Syed Raza <syed.raza011@gmail.com>
2022-11-07 08:52:11 -0500

    Question done

--- Practice/6_.js
+++ Practice/6_.js
@@ -7,6 +7,6 @@ console.log(pyramidSum([1, 4, 6])); // [[15], [5, 10], [1, 4, 6]]
 console.log(pyramidSum([3, 7, 2, 11])); // [[41], [19, 22], [10, 9, 13], [3, 7, 2, 11]]
 function pyramidSum(arr){
     for(let i=0;i<arr.length;i++){
-
+        
     }
 }
